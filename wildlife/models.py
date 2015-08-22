41ed88bc0c4b689d1e47effe052b61011ba3aa5a
2088959m <2088959m@student.gla.ac.uk>
2015-08-23 01:30:29 +0300

    just testing

--- wildlife/models.py
+++ wildlife/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
+from datetime import datetime
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(User)
+    picture = models.ImageField(upload_to='post_images')
+    description = models.TextField(blank=True)
+    date_added = models.DateTimeField()
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.date_added)
+        self.date_added = datetime.now()
+        super(Post, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return self.slug
+
+
